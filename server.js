1d81f88b8291c882be54e9176e07c254ee8b1ceb
Lesly <leslyr562@gmail.com>
2021-11-07 16:31:45 -0600

    port working

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const PORT = process.env.PORT || 3002;
 
+const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
+
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -10,13 +16,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
-    res.send('hello')
-  
+
+    res.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
+app.get('/api/notes', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "/../public/notes.html"))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.send(data);
+
+});
+
+app.get('api/notes/:id', (req, res) => {
+    res.json(data[Number(req.params.id)]);
+});
+
+
 app.post('/api/notes', (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
+    let newNote = req.body
+    console.log(newNote)
+    let noteId = (data.length).toString();
+    console.log(noteId)
+    newNote.id = noteId
+    data.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), err => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("Note submitted")
+    });
+
+    res.json(newNote)
 });
 
 
